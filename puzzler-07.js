34f593608127b5c64366d5d41ef78ffee010311f
James Young <james.young@turner.com>
2013-05-12 11:51:00 -0400

    adding another one

---
+++ puzzler-07.js
@@ -0,0 +1,57 @@
+(function () {
+    var values = [7, 4, '13', Infinity, -9];
+    for (var i = 0; i < values.length; i++) {
+        if (isOdd(values[i])) {
+            console.log(values[i]);
+        }
+    }
+}());
+
+function isOdd(num) {
+    return num % 2 == 1;
+}
+
+
+
+
+/*
+ * What does this output?
+ *
+ * a) 7, 13
+ * b) 7, 13, Infinity, -9
+ * c) 7, -9
+ * d) 7, 13, -9
+ */
+
+
+
+
+
+
+
+
+
+
+
+/*
+ * Answer: Run it and find out
+ *
+ * Why?
+ *
+ * 7 % 2        = 1    // displays
+ * 4 % 2        = 0    // does NOT display
+ * 13 % 2       = 1    // displays
+ * Infinity % 2 = NaN  // does NOT display
+ * -9 % 2       = -1   // does NOT display
+ *
+ * Javascript mod is: (same as Java)
+ * (a / b) * b + (a % b) == a
+ *
+ * side effect is the result will ALWAYS have the same sign as the left operand
+ *
+ * Solution
+ *
+ * Check for non equivalence to 0 instead of equivalence to 1
+ * Be careful about signs when using modulus operator
+ *
+ */
